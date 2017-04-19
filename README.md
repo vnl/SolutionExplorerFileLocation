b6782415224d88ad66035de7c0e235a6c58f620d
Vivian Lobo <Vivian.Lobo@ca.com>
2017-04-19 16:56:28 +0100

    Code cleanup

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Solution Explorer File Location
 
-<!-- Replace this badge with your own-->
-[![Build status](https://ci.appveyor.com/api/projects/status/hv6uyc059rqbc6fj?svg=true)](https://ci.appveyor.com/project/madskristensen/extensibilitytools)
+[![Build status](https://ci.appveyor.com/api/projects/status/5fn05kketmde0755?svg=true)](https://ci.appveyor.com/project/vnl/solutionexplorerfilelocation)
 
 <!-- Update the VS Gallery link after you upload the VSIX-->
 Download this extension from the [VS Gallery](https://visualstudiogallery.msdn.microsoft.com/[GuidFromGallery])
---
+++ SolExpFileLoc/VSPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace SolExpFileLoc
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]       
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidFileLocCommandPackageString)]
+    public sealed class VSPackage : Package
+    {
+        protected override void Initialize()
+        {
+            FileLocCommand.Initialize(this);
+            base.Initialize();
+        }
+
+    }
+}

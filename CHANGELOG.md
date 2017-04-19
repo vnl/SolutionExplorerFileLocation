c6b9b938f2f0826ef05722be0c1a68f9678d94d6
Vivian Lobo <Vivian.Lobo@ca.com>
2017-04-19 16:24:58 +0100

    Intial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Road map
+
+
+- [ ] Nothing yet...
+
+Features that have a checkmark are complete and available for
+download in the
+[CI build](http://vsixgallery.com/extension/c6775dc3-3a53-4721-a32e-81cc53f76e3f/).
+
+# Change log
+
+These are the changes to each version that has been released
+on the official Visual Studio extension gallery.
+
+
+## 1.0
+
+- [x] Initial release
+- [x] Get the name of the file from tab and shows the location of the file (Namespace)
---
+++ SolExpFileLoc/FileLocCommand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace SolExpFileLoc
+{
+    internal sealed class FileLocCommand
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("f1a36b80-a278-40b8-bbb3-25257ddce6f4");
+
+        private readonly Package package;
+
+        private FileLocCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static FileLocCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new FileLocCommand(package);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            //DTE dte = (DTE)GetService(typeof(DTE));
+            //string document = dte.ActiveDocument.FullName;
+            var dte = (DTE2) ServiceProvider.GetService(typeof(DTE));
+            string curFile = GetSelectedFile(dte);
+        }
+
+        public string GetSelectedFile(DTE2 dte)
+        {
+            string document = dte.ActiveDocument.Name;
+            return document;
+        }
+
+    }
+}
